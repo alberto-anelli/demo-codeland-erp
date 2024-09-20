3a88e4723526fc7c33a1061ae95eb2c2863b0a4c
Alessandro Giupponi <alessandro.giupponi@codeland.it>
2024-09-20 14:52:19 +0200

    Resolver refactor

---
+++ src/main/java/it/codeland/support/managementcontrol/repository/CompanyRepository.java
@@ -0,0 +1,4 @@
+package it.codeland.support.managementcontrol.model;
+
+public interface CompanyRepository extends org.springframework.data.jpa.repository.JpaRepository<it.codeland.support.managementcontrol.model.Company, java.lang.Long> {
+  }
---
+++ src/main/java/it/codeland/support/managementcontrol/resolver/CompanyResolver.java
@@ -0,0 +1,49 @@
+package it.codeland.support.managementcontrol.resolver;
+
+import it.codeland.support.managementcontrol.model.JobRole;
+import it.codeland.support.managementcontrol.repository.JobRoleRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class JobRoleResolver {
+
+    private final JobRoleRepository repository;
+
+    public JobRoleResolver(JobRoleRepository repository) {
+        this.repository = repository;
+    }
+
+    @QueryMapping
+    public JobRole jobRole(@Argument Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @QueryMapping
+    public Iterable<JobRole> allJobRoles() {
+        return repository.findAll();
+    }
+
+    @MutationMapping
+    public JobRole createJobRole(@Argument JobRole jobRole) {
+        return repository.save(jobRole);
+    }
+
+    @MutationMapping
+    public JobRole updateJobRole(@Argument JobRole jobRole) {
+        return repository.save(jobRole);
+    }
+
+    @MutationMapping
+    public Boolean deleteJobRole(@Argument Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return true;
+        } else {
+            throw new RuntimeException("JobRole not found");
+        }
+    }
+
+}
